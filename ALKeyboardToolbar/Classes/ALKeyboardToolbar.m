ff031ac1c79791c52888780a418b36ae92f2284e
Jeffrey Jackson <jj@autolean.com>
2014-01-23 02:01:06 -0500

    Refactor for initWithFrame

--- ALKeyboardToolbar/Classes/ALKeyboardToolbar.m
+++ ALKeyboardToolbar/Classes/ALKeyboardToolbar.m
@@ -1,44 +1,45 @@
-//
-//  ALKeyboardToolbar.m
-//  ALKeyboardToolbar
-//
-//  Created by Jeffrey Jackson on 1/22/14.
-//  Copyright (c) 2014 AutoLean, Inc. All rights reserved.
-//
+    //
+    //  ALKeyboardToolbar.m
+    //  ALKeyboardToolbar
+    //
+    //  Created by Jeffrey Jackson on 1/22/14.
+    //  Copyright (c) 2014 AutoLean, Inc. All rights reserved.
+    //
 
 #import "ALKeyboardToolbar.h"
 
 @implementation ALKeyboardToolbar
 @synthesize nextButton;
 
-- (id)init
+- (id)initWithFrame:(CGRect)frame
 {
-    self = [super initWithFrame:CGRectMake(0, 250, 320, 40)];
+    self = [super initWithFrame:CGRectMake(0, 250, frame.size.width, frame.size.height)];
     if (self) {
-        [self setBackgroundColor:[UIColor whiteColor]];
-        [self addTopBorderWithHeight:1.0f andColor:[UIColor colorWithRed:0.75f green:0.76f blue:0.78f alpha:1.00f]];
+        [self setBackgroundColor:[[UIColor colorWithRed:0 green:0.23 blue:0.32 alpha:1] colorWithAlphaComponent:.9]];
         [self addBottomBorderWithHeight:1.0f andColor:[UIColor colorWithRed:0.75f green:0.76f blue:0.78f alpha:1.00f]];
-        self.nextButton = [[UIButton alloc] initWithFrame:CGRectMake(self.frame.size.width - 92, 0, 92, 40)];
+        
+        self.nextButton = [[UIButton alloc] initWithFrame:CGRectMake(self.frame.size.width - 95, 1, 92, frame.size.height)];
         [self.nextButton setTitle:@"NEXT" forState:UIControlStateNormal];
-        [self.nextButton setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
-        [self.nextButton setTitleColor:[[UIColor blueColor] colorWithAlphaComponent:.3f] forState:UIControlStateHighlighted];
+        [self.nextButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        [self.nextButton setTitleColor:[[UIColor whiteColor] colorWithAlphaComponent:.3f] forState:UIControlStateHighlighted];
         [self.nextButton addTarget:self action:@selector(ALKeyboardToolbar_Next:) forControlEvents:UIControlEventTouchUpInside];
+        
         [self addSubview:self.nextButton];
     }
     return self;
 }
 
-//- (void)ALKeyboardToolbar_Next:(ALKeyboardToolbar *)_ALKeyboardToolbar {
-//    [self.delegate ALKeyboardToolbar_Next:self];
-//}
+- (void)ALKeyboardToolbar_Next:(ALKeyboardToolbar *)_ALKeyboardToolbar {
+    [self.delegate ALKeyboardToolbar_Next:self];
+}
 
 /*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
-{
-    // Drawing code
-}
-*/
+ // Only override drawRect: if you perform custom drawing.
+ // An empty implementation adversely affects performance during animation.
+ - (void)drawRect:(CGRect)rect
+ {
+ // Drawing code
+ }
+ */
 
 @end
